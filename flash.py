8ce13a26b6b18240c4e93c0f9f12b0af5a5d8da1
algarianna <arianna.gardella7@gmail.com>
2023-04-28 16:46:09 +0200

    improved script

--- flash.py
+++ flash.py
@@ -4,7 +4,6 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy import random
-from tempfile import TemporaryFile
 
 # os.system('cls')
 matplotlib.use('TkAgg')
@@ -13,42 +12,44 @@ matplotlib.use('TkAgg')
 width = 50
 height = 50
 
-t = np.arange(0, 0.1, 1)  # s
+t = np.arange(0, 1, 0.1)  # s
 pol = 1
+events_percentage = 75  # % percentage of the total number of possible events to occur at each timestamp
+num_events = round(height * width * events_percentage / 100)  # number of occurring events at each timestamp
+
 events = {"x": [], "y": [], "ts": [], "pol": [], "idx": []}
 frame = np.zeros((width, height))
+coordinates = np.zeros((width * height, 2), dtype=int)  # list of all pixel coordinates
+idx = 0
+for x in np.arange(width):
+    for y in np.arange(height):
+        coordinates[idx][0] = x
+        coordinates[idx][1] = y
+        idx += 1
+
 plt.figure()
-num_e = 2000
-
 for ts in t:
-    x_sample = np.random.choice(np.arange(width), size=num_e)
-    y_sample = [random.randint(height)]
-    for i in np.arange(num_e - 1) + 1:
-        y_tmp = random.randint(height)
-        if y_tmp in y_sample and x_sample[y_sample.index(y_tmp)] == x_sample[i]:
-            i = i-2
-        else:
-            y_sample.append(y_tmp)
-
-    for i in np.arange(len(x_sample)):
+    coor_idx = np.random.choice(len(coordinates), size=num_events, replace=False)
+    coor_sample = coordinates[coor_idx]
+    for i in np.arange(len(coor_sample)):
         # events is a list of tuples: (x position, y position, time in seconds, on/off polarity)
         # creating events
         # x
-        events['x'].append(x_sample[i])
+        events['x'].append(coor_sample[i][0])
         # y
-        events['y'].append(y_sample[i])
+        events['y'].append(coor_sample[i][1])
         # ts
         events['ts'].append(ts)
         # pol
         events['pol'].append(pol)
         # idx
-        events['idx'].append(x_sample[i] * height + y_sample[i])
-        frame[y_sample[i], x_sample[i]] = 1
-    # Visualization
-    # plt.ion()
+        events['idx'].append(coor_sample[i][0] * height + coor_sample[i][1])
+        frame[coor_sample[i][1], coor_sample[i][0]] = 1
+
+ # Visualization
     plt.imshow(frame)
     plt.draw()
-    plt.pause(0.5)
+    plt.pause(0.1)
     frame = np.zeros((width, height))
 
-np.save("new_events.npy", events)
+np.save("events.npy", events)
