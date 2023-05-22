c0e3bf85809129e0fbffff65e706c00d7eef7990
algarianna <arianna.gardella7@gmail.com>
2023-05-22 19:10:15 +0200

    Events from moving grating

--- grating.py
+++ grating.py
@@ -9,45 +9,42 @@ matplotlib.use('TkAgg')
 # camera resolution
 width = 50
 height = 50
-v = 5  # px/s
-t_period = 0.1  # s
+v = 1  # px/s
+t_period = 1/v  # s
 l: int = 5
 
-# white bar on dark background
-pol = [1, 0]
 # dark bar on white background
 pol = [0, 1]
 idx = 0
 y_array = np.arange(height)
 events = {"x": [], "y": [], "ts": [], "pol": [], "idx": []}
-for x in np.arange(l):
-    # np.random.shuffle(y_array)
-    for y in y_array:
+N = 5  # n. of bars
+# 100 events happening within each time period
+# v = 10 and l = 5 so t_bar_window = 0.5 s
+for x in np.arange(width):
+    coord: list = []
+    t = x / v  # starting time-point at each shift but probably useless
+    for i in np.arange(N):
+        for y in y_array:
+            coord.append((x + 2*i*l, y))
+        if l < (x + 2*i*l):
+            for yy in y_array:
+                coord.append((((x + 2*i*l)-l), yy))
+    np.random.shuffle(coord)
+    time_window = np.round(np.arange(0, (1 / v), ((1 / v) / len(coord))), 3)
+    for i in np.arange(len(coord)):
         # events is a list of tuples: (x position, y position, time in seconds, on/off polarity)
-        t = x / v  # s
         # creating events
-        for i in np.arange(np.floor(width/(2*l)), dtype=int):
-            # x
-            events['x'].append((x + i*2*l))
-            # y
-            events['y'].append(y)
-            # ts
-            events['ts'].append(t)
-            # pol
-            events['pol'].append(pol[1])
-            # idx
-            events['idx'].append((x + i*l) * height + y)
-            # if l < x + i*2*l < (width - l):
-            #     # x
-            #     events['x'].append(x + i*2*l - l)
-            #     # y
-            #     events['y'].append(y)
-            #     # ts
-            #     events['ts'].append(t)
-            #     # pol
-            #     events['pol'].append(pol[0])
-            #     # idx
-            #     events['idx'].append((x + i*2*l - l) * height + y)
+        # x
+        events['x'].append(coord[i][0])
+        # y
+        events['y'].append(coord[i][1])
+        # ts
+        events['ts'].append(t + time_window[i])
+        # pol
+        events['pol'].append(pol[1])
+        # idx
+        events['idx'].append(coord[i][0] * height + coord[i][1])
 
 # outfile = TemporaryFile()
 np.save("events.npy", events)
@@ -66,7 +63,7 @@ for idx in np.arange(len(events['x'])):
     else:
         ax.matshow(frame)  # or ax.imshow(frame)
         plt.draw()
-        plt.pause(0.01)
+        plt.pause(0.2)
         t_period_state += t_period
         frame = np.zeros((height, width))
         frame[(events['y'][idx], events['x'][idx])] = 1
