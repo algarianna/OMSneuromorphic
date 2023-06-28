b27cf96d8ffd31e1899fc5f93dc4cea329a55b9b
algarianna <arianna.gardella7@gmail.com>
2023-06-28 18:12:27 +0200

    Incoherent motion stimulus at saccade speed

--- random_single_bar.py
+++ random_single_bar.py
@@ -12,8 +12,8 @@ matplotlib.use('TkAgg')
 # camera resolution
 width = 50
 height = 50
-v = 10  # px/s
-t_period = 0.1  # s
+v = 142  # px/s  given the max saccade latency at 350 ms and the frame width at 50 px
+t_period = 1/v  # s
 l: int = 5
 
 # white bar on dark background
@@ -26,59 +26,49 @@ events = {"x": [], "y": [], "ts": [], "pol": [], "idx": []}
 # 100 events happening within each time period
 # v = 10 and l = 5 so t_bar_window = 0.5 s
 for x in np.arange(width):
-    np.random.shuffle(y_array)
-    if l < x < (width - l):
-        time_window = np.arange(0, (1 / v), ((1 / v) / 2 * height))  # 100 timepoints for 100 events
-    else:
-        time_window = np.arange(0, (1 / v), ((1 / v) / height))  # 50 timepoints for 50 events
-    np.random.shuffle(time_window)
+    coord: list = []
     t = x / v  # starting time-point at each shift but probably useless
-    dt = 0  # index to get time_window timepoints
     for y in y_array:
+        coord.append((x, y))
+    if l < x < (width - l):
+        time_window = np.round(np.arange(0, t_period, (t_period / (2 * height))), 3)  # 100 timepoints for 100 events
+        for yy in y_array:
+            coord.append(((x-l), yy))
+    else:
+        time_window = np.round(np.arange(0, (1 / v), ((1 / v) / height), dtype = float ), 3)  # 50 timepoints for 50 events
+    np.random.shuffle(coord)
+    for i in np.arange(len(coord)):
         # events is a list of tuples: (x position, y position, time in seconds, on/off polarity)
         # creating events
         # x
-        events['x'].append(x)
+        events['x'].append(coord[i][0])
         # y
-        events['y'].append(y)
+        events['y'].append(coord[i][1])
         # ts
-        events['ts'].append(t + time_window[dt])
+        events['ts'].append(t + time_window[i])
         # pol
         events['pol'].append(pol[1])
         # idx
-        events['idx'].append(x * height + y)
-        dt += 1
-        if l < x < (width - l):
-            # x
-            events['x'].append(x - l)
-            # y
-            events['y'].append(y)
-            # ts
-            events['ts'].append(t + time_window[dt])
-            # pol
-            events['pol'].append(pol[1])
-            # idx
-            events['idx'].append((x - l) * height + y)
-            dt += 1
+        events['idx'].append(coord[i][0] * height + coord[i][1])
 
 # outfile = TemporaryFile()
 np.save("events.npy", events)
 # events_np = np.load("events.npy", allow_pickle='TRUE').item()
 # print(events_np)
 # Visualization
-frame = np.zeros((height, width))
-t_period_state = t_period
-plt.ion()
-fig = plt.figure()
-ax = fig.add_subplot(111)
-
-for idx in np.arange(len(events['x'])):
-    if events['ts'][idx] < t_period_state:
-        frame[(events['y'][idx], events['x'][idx])] = 1
-    else:
-        ax.matshow(frame)  # or ax.imshow(frame)
-        plt.draw()
-        plt.pause(0.001)
-        t_period_state += t_period
-        frame = np.zeros((height, width))
-        frame[(events['y'][idx], events['x'][idx])] = 1
+# frame = np.zeros((height, width))
+# t_period_state = t_period
+# plt.ion()
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+#
+# for idx in np.arange(len(events['x'])):
+#     if events['ts'][idx] < t_period_state:
+#         frame[(events['y'][idx], events['x'][idx])] = 1
+#     else:
+#         ax.matshow(frame)  # or ax.imshow(frame)
+#         plt.draw()
+#         plt.pause(0.2)
+#         t_period_state += t_period
+#         frame = np.zeros((height, width))
+#         frame[(events['y'][idx], events['x'][idx])] = 1
